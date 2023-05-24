f4d20a7f87b7d71f147803424a1940731fe55dbe
OmerMasih <you@example.com>
2023-05-23 20:59:23 -0700

    establish a connection to a MongoDB database using Mongoose

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,3 +3,14 @@ const mongoose = require("mongoose");
 
 // Mongoose to allow querying on properties that are not defined in the schema.
 mongoose.set("strictQuery", false);
+
+// establish a connection to a MongoDB database using Mongoose.
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/MedicationTracker",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+module.exports = mongoose.connection;
