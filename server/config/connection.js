d7553546c3303e6704eaae07a39d1c7031ad4761
OmerMasih <you@example.com>
2023-05-23 20:11:04 -0700

    Mongoose to allow querying on properties that are not defined in the schema

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,1 +1,5 @@
+//Sitting my connections
 const mongoose = require("mongoose");
+
+// Mongoose to allow querying on properties that are not defined in the schema.
+mongoose.set("strictQuery", false);
