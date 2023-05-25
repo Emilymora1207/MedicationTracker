cb91e3149d8108436fe33e2eb2a6a5fd9ac46b70
OmerMasih <masihomer123@gmail.com>
2023-05-24 19:16:59 -0700

    Creating the auth.js file and importing the jsonwebtoken and dotenv

---
+++ server/utils/auth.js
@@ -0,0 +1,4 @@
+//  import the jsonwebtoken module in a Node.js application.
+const jwt = require("jsonwebtoken");
+// configure environment variables from a .env file in a Node.js application.
+require("dotenv").config();
