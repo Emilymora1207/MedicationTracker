20e7a92c63ed27940ca1946a7f8281d4dc9505b6
OmerMasih <masihomer123@gmail.com>
2023-05-25 19:23:53 -0700

    Sitting the function that takes two arguments: timestamp and an options object that contains monthLength and dateSuffix properties

--- server/utils/dateFormat.js
+++ server/utils/dateFormat.js
@@ -16,3 +16,26 @@ const addDate = (date) => {
 
   return dateFormat;
 };
+
+// Sitting the function that takes two arguments: timestamp and an options object that contains monthLength and dateSuffix properties.
+
+module.exports = (
+  timestamp,
+  { monthLength = "short", dateSuffix = true } = {}
+) => {
+  // create month object
+  const months = {
+    0: monthLength === "short" ? "Jan" : "January",
+    1: monthLength === "short" ? "Feb" : "February",
+    2: monthLength === "short" ? "Mar" : "March",
+    3: monthLength === "short" ? "Apr" : "April",
+    4: monthLength === "short" ? "May" : "May",
+    5: monthLength === "short" ? "Jun" : "June",
+    6: monthLength === "short" ? "Jul" : "July",
+    7: monthLength === "short" ? "Aug" : "August",
+    8: monthLength === "short" ? "Sep" : "September",
+    9: monthLength === "short" ? "Oct" : "October",
+    10: monthLength === "short" ? "Nov" : "November",
+    11: monthLength === "short" ? "Dec" : "December",
+  };
+};
